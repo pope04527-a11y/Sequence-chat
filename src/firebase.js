6c52a75487558cce99c4a2dbfbe5c63e010698c0
Tiffa256 <tiffashan10@gmail.com>
2025-11-30 23:39:04 +0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeJhHkhCmsCUe5nFLEb6ey5KruAsNFNuQ",
+  authDomain: "stacks-chat-b795c.firebaseapp.com",
+  databaseURL: "https://stacks-chat-b795c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stacks-chat-b795c",
+  storageBucket: "stacks-chat-b795c.firebasestorage.app",
+  messagingSenderId: "410462423292",
+  appId: "1:410462423292:web:48dbeb3d6a5149952b2f79"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
