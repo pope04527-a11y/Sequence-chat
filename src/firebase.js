24cbf55092877a6441d63baf02a81b60b10d85b9
Tiffa256 <tiffashan10@gmail.com>
2025-12-01 00:25:56 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDeJhHkhCmsCUe5nFLEb6ey5KruAsNFNuQ",
   authDomain: "stacks-chat-b795c.firebaseapp.com",
-  databaseURL: "https://stacks-chat-b795c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "stacks-chat-b795c",
-  storageBucket: "stacks-chat-b795c.firebasestorage.app",
+  storageBucket: "stacks-chat-b795c.appspot.com",
   messagingSenderId: "410462423292",
   appId: "1:410462423292:web:48dbeb3d6a5149952b2f79"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
