9bd755b5ce03daad54037bbe9192e378044a7761
Tiffa256 <tiffashan10@gmail.com>
2025-12-01 16:16:31 +0800

    Update AdminMessages.js

--- src/admin/AdminMessages.js
+++ src/admin/AdminMessages.js
@@ -1,1 +1,9 @@
+export default function AdminMessages() {
+  return (
+    <div className="admin-container">
+      <h2 className="admin-title">Messages</h2>
+      <p>Here you will see all platform messages.</p>
+    </div>
+  );
+}
 
