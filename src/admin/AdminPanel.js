56d17018de648ca181670d4b0772590deeaa3ad6
Tiffa256 <tiffashan10@gmail.com>
2025-12-04 21:47:07 +0800

    Update AdminPanel.js

--- src/admin/AdminPanel.js
+++ src/admin/AdminPanel.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import AdminChat from "./AdminChat";
-import "./AdminChat.css";
+import "./Admin.css";
 import { db } from "../firebase";
 
 function AdminPanel() {
